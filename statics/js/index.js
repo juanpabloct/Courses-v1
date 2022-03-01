b4786b47af4da713a05b4b4a3f285d89a345b05b
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-03-01 17:35:14 -0500

    Se creo la carpeta statics para que contenga todos los archivos estaticos

---
+++ statics/js/index.js
@@ -0,0 +1,15 @@
+const allCourses = document.querySelector(".courses");
+const mayor = document.getElementById("mayor");
+const programas = document.getElementById("programas");
+allCourses.addEventListener(
+  "mouseenter",
+  () => (mayor.style.transform = "rotate(90deg)")
+);
+allCourses.addEventListener(
+  "mouseleave",
+  () => (mayor.style.transform = "rotate(0deg)")
+);
+const cursos = ["Javascript", "Python", "Django", "React"];
+let valor = "";
+cursos.forEach((e) => (valor += `<span>${e}</span>`));
+programas.innerHTML = valor;
