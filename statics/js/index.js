7047e708f1832568ab36c9e275351659639114a3
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-03-01 17:58:26 -0500

    Se añadio la caracteristica a las categorias de none cuando no hay mouse encima  y al colocarlo se desplegan

--- statics/js/index.js
+++ statics/js/index.js
@@ -1,14 +1,12 @@
 const allCourses = document.querySelector(".courses");
 const mayor = document.getElementById("mayor");
 const programas = document.getElementById("programas");
-allCourses.addEventListener(
-  "mouseenter",
-  () => (mayor.style.transform = "rotate(90deg)")
-);
-allCourses.addEventListener(
-  "mouseleave",
-  () => (mayor.style.transform = "rotate(0deg)")
-);
+allCourses.addEventListener("mouseenter", () => {
+  mayor.style.transform = "rotate(90deg)";
+});
+allCourses.addEventListener("mouseleave", () => {
+  mayor.style.transform = "rotate(0deg)";
+});
 const cursos = ["Javascript", "Python", "Django", "React"];
 let valor = "";
 cursos.forEach((e) => (valor += `<span>${e}</span>`));
